9aa56914ebf755b5b398a72642ce8d6a54a7a39d
apmckee42 <apmkee42@students.tntech.edu>
2020-09-30 01:01:42 -0500

    Login Page update

--- EagleLife/EagleLife/Login.aspx.cs
+++ EagleLife/EagleLife/Login.aspx.cs
@@ -21,7 +21,40 @@ namespace EagleLife
 
         protected void btnLogin_Click(object sender, EventArgs e)
         {
-            
+            if (txtPassword.Text != "" && txtPassword.Text != "")
+            {
+
+
+                SqlConnection connect = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename =D:\GitHub\eaglelife\EagleLifeLogin.mdf; Integrated Security = True; Connect Timeout = 30");
+                string selectState = "Select adminID, adminUsername, adminPassword " +
+                                     "From AdminLogin where adminID = @adminID AND adminPassword = @adminPassword";
+
+                SqlCommand cmd = new SqlCommand(selectState, connect);
+
+                cmd.Parameters.AddWithValue("@adminID", txtUsername.Text);
+                cmd.Parameters.AddWithValue("@adminPassword", txtPassword.Text);
+                connect.Open();
+
+                int count = Convert.ToInt32(cmd.ExecuteScalar());
+
+                if (count == 1)
+                {
+                    connect.Close();
+                    Session["adminUsername"] = txtUsername.Text.Trim();
+                    Response.Redirect("Initial.aspx");
+                }
+
+                else
+                {
+                    lblLoginError.Visible = true;
+                }
+            }
+            else
+            {
+                lblLoginError.Visible = true;
+
+            }
+
         }
     }
 }

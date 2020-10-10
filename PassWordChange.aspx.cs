4f2b3f6c2a1d8cae2095a88e306a7733eba2af40
apmckee42 <71366079+apmckee42@users.noreply.github.com>
2020-10-09 23:51:09 -0500

    Forgot and change password updates

--- PassWordChange.aspx.cs
+++ PassWordChange.aspx.cs
@@ -20,6 +20,7 @@ namespace EagleLife
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            lblSuccess.Visible = false;
             lblChange.Visible = false;
             ChangePassword1.ChangingPassword += new LoginCancelEventHandler(this.ChangePassword1_ChangedPassword);
 
@@ -27,7 +28,41 @@ namespace EagleLife
 
         protected void ChangePassword1_ChangedPassword(object sender, EventArgs e)
         {
-            
+            if(!ChangePassword1.CurrentPassword.Equals(ChangePassword1.NewPassword, StringComparison.CurrentCultureIgnoreCase))
+            {
+                int rowsaffected = 0;
+
+                string query = "Update [AdminLogin] Set [Password] = @NewPassword where [AdminUser] = @AdminUser AND [AdminPass] = @AdminPass";
+
+                string constr = ConfigurationManager.ConnectionStrings["EagleLifeDBConnectionString"].ConnectionString;
+
+                using(SqlConnection con = new SqlConnection(constr))
+                {
+                    using(SqlCommand cmd = new SqlCommand(query))
+                    {
+                        using (SqlDataAdapter sda = new SqlDataAdapter())
+                        {
+                            cmd.Parameters.AddWithValue("@AdminUser", this.Page.User.Identity.Name);
+                            cmd.Parameters.AddWithValue("@AdminPass", ChangePassword1.CurrentPassword);
+                            cmd.Parameters.AddWithValue("@NewPassword", ChangePassword1.NewPassword);
+                            cmd.Connection = con;
+                            con.Open();
+                            rowsaffected = cmd.ExecuteNonQuery();
+                            con.Close();
+                          
+                        }
+                    }
+                    if(rowsaffected > 0)
+                    {
+                        lblSuccess.Visible = true;
+                    }
+                    else
+                    {
+                        lblChange.Visible = true;
+                    }
+                    
+                }
+            }
         }
     }
 }
--- PassWordChange.aspx.designer.cs
+++ PassWordChange.aspx.designer.cs
@@ -33,6 +33,15 @@ namespace EagleLife
         protected global::System.Web.UI.WebControls.ChangePassword ChangePassword1;
 
         /// <summary>
+        /// lblSuccess control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblSuccess;
+
+        /// <summary>
         /// lblChange control.
         /// </summary>
         /// <remarks>

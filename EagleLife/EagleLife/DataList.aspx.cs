c66d7cd5546317f1ec389faca89abefb321e0a7a
secampbell42 <71193668+secampbell42@users.noreply.github.com>
2020-10-13 20:09:31 -0500

    Save Button Worked On

--- EagleLife/EagleLife/DataList.aspx.cs
+++ EagleLife/EagleLife/DataList.aspx.cs
@@ -8,14 +8,17 @@ using System.Data.SqlClient;
 using System.Data;
 using System.Configuration;
 using System.Text;
+using System.IO;
+using System.Data.Common;
 
 namespace EagleLife
 {
     public partial class DataList : System.Web.UI.Page
     {
 
-        private char csv;
-        private object dataList;
+        //internal object Column;
+        //internal object Columns;
+        //internal readonly IEnumerable<DataRow> Rows;
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -27,117 +30,131 @@ namespace EagleLife
             Response.Redirect("~/Default.aspx");
         }
 
+
+
         protected void btnSaveFile_Click(object sender, EventArgs e)
         {
-            var dataTable = GetData(dataList);
-            StringBuilder builder = new StringBuilder();
-            List<string> columnNames = new List<string>();
-            List<string> rows = new List<string>();
+            ////DataList dl = OperationsUtlity.CreateDataList();
+            //string filename = DataList.OpenSavefileDialog();
+            //DataList.ToCSV(filename);
 
-            System.Collections.IList list = columnNames;
-            for (int i = 0; i < list.Count; i++)
-            {
-                DataColumn column = (DataColumn)list[i];
-                columnNames.Add(column.ColumnName);
-            }
+           
 
-            builder.Append(string.Join(",", columnNames.ToArray())).Append("\n");
-
-            System.Collections.IList list1 = rows;
-            for (int i = 0; i < list1.Count; i++)
-            {
-                DataRow row = (DataRow)list1[i];
-                List<string> currentRow = new List<string>();
-
-                System.Collections.IList list2 = columnNames;
-                for (int i1 = 0; i1 < list2.Count; i1++)
-                {
-                    DataColumn column = (DataColumn)list2[i1];
-                    object item = row[column];
-
-                    currentRow.Add(item.ToString());
-                }
-
-                rows.Add(string.Join(",", currentRow.ToArray()));
-            }
-
-            builder.Append(string.Join("\n", rows.ToArray()));
-
-            Response.Clear();
-            Response.Buffer = true;
-            Response.AddHeader("Content-Disposition", "attachment;filename=myfilename.csv");
-            Response.Charset = "";
-            Response.ContentType = "text/csv";
-            Response.Output.Write(builder.ToString());
-            Response.Flush();
+            Response.ContentType = "application/octet-stream";
+            Response.AppendHeader("Content-Disposition", @"attachment; filename=""export.csv""");
+            Response.TransmitFile(Server.MapPath("~/TestFile.aspx"));
+            //Response.Write(sb.ToString());
             Response.End();
-            //-------------------------------------------------------------
-            //string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
-            //using (SqlConnection con = new SqlConnection(constr))
-            //{
-            //    using (SqlCommand cmd = new SqlCommand("SELECT * FROM Student"))
-            //    {
-            //        using (SqlDataAdapter sda = new SqlDataAdapter())
-            //        {
-            //            cmd.Connection = con;
-            //            sda.SelectCommand = cmd;
-            //            using (DataTable dt = new DataTable())
-            //            {
-            //                sda.Fill(dt);
 
-            //                //Build the CSV file data as a Comma separated string.
-            //                string csv = string.Empty;
 
-            //                foreach (DataColumn column in dt.Columns)
-            //                {
-            //                    //Add the Header row for CSV file.
-            //                    csv += column.ColumnName + ',';
-            //                }
-
-            //                //Add new line.
-            //                csv += "\r\n";
-
-            //                foreach (DataRow row in dt.Rows)
-            //                {
-            //                    foreach (DataColumn column in dt.Columns)
-            //                    {
-            //                        //Add the Data rows.
-            //                        csv += row[column.ColumnName].ToString().Replace(",", ";") + ',';
-            //                    }
-
-            //                    //Add new line.
-            //                    csv += "\r\n";
-            //                }
-
-            //                //Download the CSV file.
-            //                Response.Clear();
-            //                Response.Buffer = true;
-            //                Response.AddHeader("content-disposition", "attachment;filename=SqlExport.csv");
-            //                Response.Charset = "";
-            //                Response.ContentType = "application/text";
-            //                Response.Output.Write(csv);
-            //                Response.Flush();
-            //                Response.End();
-            //            }
-            //        }
-            //    }
-            //    }
-            //}
-
-            //protected void btnSaveFile_Click1(object sender, EventArgs e)
-            //{
-
-            //}
-
-            //protected void EagleLifeDB_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
-            //{
-
-            //}
         }
 
-        private object GetData(object dataList)
+        //private static void ToCSV(string filename)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //private static string OpenSavefileDialog()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        protected void btnSaveFile_Click1(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            
+
+
         }
+        protected void EagleLifeDB_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+        {
+
+        }
+
     }
 }
+    //public static class OperationsUtlity
+    //{
+    //    public static DataTable CreateDataTable()
+    //    {
+    //        //get
+    //        //{
+    //            DataTable dt = new DataTable();
+    //            dt.Columns.Add("Id", typeof(int));
+    //            dt.Columns.Add("Name", typeof(string));
+    //            dt.Columns.Add("Email", typeof(string));
+    //            dt.Columns.Add("Phone", typeof(string));
+    //            dt.Columns.Add("School", typeof(string));
+    //            dt.Columns.Add("Leader", typeof(string));
+    //            dt.Columns.Add("Has_Code", typeof(string));
+    //            dt.Columns.Add("Group_Code",typeof(string));
+    //        //Get Rows From Dataset
+    //        SqlDataAdapter da = new SqlDataAdapter();
+    //        DataSet ds = new DataSet();
+    //        da.SelectCommand = new SqlCommand(@"SELECT * FROM Student", DbConnectionStringBuilder);
+    //        da.Fill(ds, "Student");
+    //        dt = ds.Tables["Student"];
+    //        //dt.Rows.Add(2, "Mudassar Khan", "India");
+    //        //dt.Rows.Add(3, "Suzanne Mathews", "France");
+    //        //dt.Rows.Add(4, "Robert Schidner", "Russia");
+
+    //        //DataList dl = new DataList();
+    //        //dt.BorderStyle = BorderStyle.Groove;
+
+    //        //dl.ItemTemplate = new DatalistLabelColumnBind();
+    //        //dl.DataSource = dt;
+    //        //dl.DataBind();
+
+    //        //return table;
+    //    }
+
+
+    //    internal static DataList CreateDataList()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+
+//    public static class CSVUtlity
+//    {
+
+//        public static void ToCSV(this Page dtDataTable, string strFilePath)
+//        {
+//            StreamWriter sw = new StreamWriter(strFilePath, false);
+//            //headers  
+//            for (int i = 0; i < dtDataTable.Column.Count; i++)
+//            {
+//                sw.Write(dtDataTable.Columns[i]);
+//                if (i < dtDataTable.Columns.Count - 1)
+//                {
+//                    sw.Write(",");
+//                }
+//            }
+//            sw.Write(sw.NewLine);
+//            foreach (DataRow dr in dtDataTable.Rows)
+//            {
+//                for (int i = 0; i < dtDataTable.Columns.Count; i++)
+//                {
+//                    if (!Convert.IsDBNull(dr[i]))
+//                    {
+//                        string value = dr[i].ToString();
+//                        if (value.Contains(','))
+//                        {
+//                            value = String.Format("\"{0}\"", value);
+//                            sw.Write(value);
+//                        }
+//                        else
+//                        {
+//                            sw.Write(dr[i].ToString());
+//                        }
+//                    }
+//                    if (i < dtDataTable.Columns.Count - 1)
+//                    {
+//                        sw.Write(",");
+//                    }
+//                }
+//                sw.Write(sw.NewLine);
+//            }
+//            sw.Close();
+//        }
+//    }
+//}
